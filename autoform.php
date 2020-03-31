5735eaa5473e703c45420806a7584b0e96a414fc
Pierre <pierre.decrock@gmail.com>
2020-03-31 16:17:21 +0200

    init

---
+++ autoform.php
@@ -0,0 +1,58 @@
+<?php
+
+class autoform {
+
+	public function getInputText($label, $name){
+
+		echo '<p><label for="'.$name.'">'.$label.' : </label>';
+		echo '<input type="text" id="'.$name.'" name="'.$name.'"></p>';
+
+	}
+
+	public function getInputSubmit($label){
+
+		echo '<input type="submit" id="'.$label.'" name="'.$label.'"><br>';
+
+	}
+
+	public function getInputList($label, $name, $values){
+
+		echo '<p><label for="'.$name.'">'.$label.' : </label><br>';
+		echo '<select id="'.$name.'" name="'.$name.'">';
+		foreach ($values as $value) {
+			echo '<option value="'.$value.'">'.$value.'</option>';
+		}
+		echo "</select></p>";
+		
+	}
+
+	public function getInputRadio($label, $name, $values){
+		echo '<p><label for="'.$name.'">'.$label.' : </label> ';
+		foreach ($values as $value){
+			echo '<label for="'.$name.'">'.$value.'</label>';
+			echo '<input type="radio" id="'.$name.'" name="'.$name.'" value="'.$value.'">';
+		}
+	}
+
+	public function getInputPassword($label, $name){
+		echo '<p><label for="'.$name.'">'.$label.' : </label> ';
+		echo '<input type="password" id="'.$name.'" name="'.$name.'" minlength="8" required></p>';
+	}
+
+	public function getInputDate($label, $name){
+		echo '<p><label for="'.$name.'">'.$label.' : </label> ';
+		echo '<input type="date" id="'.$name.'" name="'.$name.'" min="2018-01-01" max="2018-12-31"></p>';
+	}
+
+	public function getInputTextArea($label, $name){
+		echo '<p><label for="'.$name.'">'.$label.' : </label></p>';
+		echo '</p><textarea id="'.$name.'" name="'.$name.'" rows="5" cols="33"></textarea></p>';
+	}
+
+
+
+
+
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="MonSite_PierreDecrock/style.css"/>
+		<title>PHP</title>
+	</head>
+	<body>
+		<div id="bloc">
+			<header>
+				<div id="titre_principal">
+					<div id="logo">
+						<h1>Groupe 5</h1>
+					</div>
+				</div>
+				<nav>
+					<ul>
+						<li><a href="#exos">Exos</a></li>
+						<li><a href="#algo">Algo</a></li>
+						<li><a href="#bdd">BDD</a></li>
+						<li><a href="#laposte">LaPoste</a></li>
+					</ul>
+				</nav>
+			</header>
+
+			<div id="couverture">
+				<div id="description_couverture">
+				</div>
+			</div>
+
+			<section>
+				<article>
+					<div id="exos">
+						<h1>
+							Exos
+						</h1>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+						</p>
+					</div>
+					<div id="algo">
+						<h1>
+							Algo
+						</h1>
+						<p>
+							<?php 
+								require "dinosaure.php";
+								require "autoform.php";
+								/*$dino = new tyrex("555", "40", 4, "Méga Tyrex","m", "1", 4);
+								$trex = new tyrex("333", "30", 3, "Mini Tyrex","m", "2", 2);
+
+								echo "<p>Combat entre 2 tyrex</p>";
+								while (true) {
+									if ($dino->getLife()>0) {
+										$dino->makeSeism("true", $trex);
+									}
+									else {
+										echo "<p>".$dino->getName()." est mort</p>";
+										break;
+									}
+									if ($trex->getLife()>0) {
+										$trex->makeSeism("true", $dino);
+									}
+									else {
+										echo "<p>".$trex->getName()." est mort</p>";
+										break;
+									}
+									
+								}
+								$ptero = new pterodactyle (222, 35, 3,"Moyen ptéro", "f", "3m", 2);
+								echo "<p>Combat entre 1 tyrex et 1 Ptero</p>";
+								// Manque la condition pour savoir si le duel est possible : si dino aérien et l'autre terrestre, pas de combat ou alors une nouvelle attaque, propre à TOUS les dinos (exemple : vharge classique indéxée sur les damages)
+								// j'essaierai de la faire demain matin 
+								while (true) {
+									if ($dino->getLife()>0) {
+										$dino->makeSeism("true", $ptero);
+									}
+									else {
+										echo "<p>".$dino->getName()." est mort</p>";
+										break;
+									}
+									if ($ptero->getLife()>0) {
+										$ptero->makeTornado("true", $dino);
+									}
+									else {
+										echo "<p>".$ptero->getName()." est mort</p>";
+										break;
+									}
+								}*/
+							?>
+							<form method="POST" action="#">
+								<?php
+									$form = new autoform;
+
+									$form->getInputText("Nom", "name");
+
+									$form->getInputRadio("Sexe","gender", ["M","F"]);
+
+									$form->getInputList("Points de vie","life", ["20","30","50","100"]);
+
+									$form->getInputPassword("Mot de passe", "password");
+
+									$form->getInputDate("Date", "date");
+
+									$form->getInputTextArea("Ecrivez du texte par ici", "textarea");
+
+									$form->getInputSubmit("Valider");
+								?>
+								
+							</form>
+							
+							<?php
+
+								if (!empty($_POST)) {
+									var_dump($_POST);
+									//$length, $life, $damage, $name, $gender, $nbrArms, $sizeArms
+									$toto = new tyrex("",$_POST["life"],"",$_POST["name"],$_POST["gender"],"","");
+								}
+								
+							?>
+						</p>
+					</div>
+					<div id="bdd">
+						<h1>
+							Base de données
+						</h1>
+						<p>
+							Ma base de données
+						</p>
+					</div>
+					<div id="laposte">
+						<h1>
+							La poste
+						</h1>
+						<p>
+							Vive les facteurs
+						</p>
+					</div>
+
+					
+				</article>
+			</section>
+
+			<footer>
+				<div id="groupe">
+					<h1>Mon groupe</h1>
+					<ul>
+						<li>Anis Bastide</li>
+						<li>Jimmy Lai</li>
+						<li>Dorian Lebourgeois</li>
+						<li>Pierre Decrock</li>
+					</ul>
+					
+				</div>
+				<div id="rien">
+					<p>
+						
+					</p>
+					
+				</div>
+				<div id="Contact">
+					<h1>Me contacter</h1>
+					<ul>
+						<li>Téléphone : 06.12.34.56.78</li>
+						<li>Mail : pierre.decrock@gmail.com</li>
+					</ul>
+					
+				</div>
+
+			</footer>
+		</div>
+	</body>		
+</html>
