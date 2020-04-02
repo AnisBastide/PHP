f104e0e0f328163a85ff8e5c1d7c96a6ff420a89
Pierre <pierre.decrock@gmail.com>
2020-04-02 19:18:01 +0200

    template

--- portions/laposte.php
+++ portions/laposte.php
@@ -5,30 +5,43 @@
 	<link rel="stylesheet" href="style.css"/>
 </head>
 <body>
-	<?php include ("header.php"); ?>
-
-	<h2>La Poste</h2>
-
-	<?php
+	<?php 
+		include ("header.php");
 		require "../autoform.php";
 		require "../request.php";
 
-		$form = new autoform();
-		$req = new request('root','root','php', 'mysql','localhost');
-		$form->getInputList("Expéditeur", "expeditor", $req->getColumns("contact", "lastname"));
-		$form->getInputList("Destinataire", "dest", $req->getColumns("contact", "lastname"));
-
-
-
 	?>
 
-	<a href="template.php">Preview</a>
+	<h2>La Poste</h2>
 
+	<form method="POST" action="template.php" target="_BLANK">
+		<?php
 
+		$form = new autoform();
+		$req = new request('root','root','php', 'mysql','localhost');
 
+		?>
+		<h4>Expéditeur : </h4>
+		<?php
+		$form->getInputText('Nom', 'lastnameExp');
+		$form->getInputText('Prénom', 'firstnameExp');
+		$form->getInputText('Adresse', 'adressExp');
+		$form->getInputText('Adresse mail', 'mailExp');
+		// $form->getInputList("Expéditeur", "expeditor", $req->getColumns("contact", "lastname"));
+		// $form->getInputList("Destinataire", "dest", $req->getColumns("contact", "lastname"));
+		?>
+		<h4>Destinataire : </h4>
+		<?php
+		$form->getInputText('Nom', 'lastnameDest');
+		$form->getInputText('Prénom', 'firstnameDest');
+		$form->getInputText('Adresse', 'adressDest');
+		$form->getInputText('Adresse mail', 'mailDest');
 
-
-
+		$form->getInputRadio("Envoi prioritaire", "priority", ["Oui", "Non"]);
+		$form->getInputRadio("Envoi confidentiel", "confidential", ["Oui", "Non"]);
+		$form->getInputSubmit("Valider");
+		?>
+	</form>
 	<?php include ("footer.php"); ?>
 
 </body>
--- portions/template.php
+++ portions/template.php
@@ -6,37 +6,37 @@
 </head>
 
 <body>
-	<h1>Preview lettre</h1>
-	<section id="prioAndCachet">
-		<div id="priority">
-			<img class="logo" src="timbreRouge.jpg" alt="timbre rouge">
-			<!-- <?php
-				if (checkbox) {
-				 	echo '<img class="logo" src="timbreRouge.jpg" alt="timbre rouge">';
-				 }
-				else {
-					echo '<img class="logo" src="timbreVert.jpg" alt="timbre vert">';
-				}
-			?> -->
+	<h1>Aperçu de la lettre</h1>
+	<section id="lettre">
+		<section id="prioAndCachet">
+			<div id="cachet">
+				<?php 
+					if ($_POST["confidential"]=="Oui") {
+						echo '<img class="logo" src="confidential.png" alt="Tampon confidentiel">';
+					}
+				?>	
+			</div>
+			<div id="priority">
+				<?php
+					if ($_POST["priority"]=="Oui") {
+					 	echo '<img class="logo" src="timbreRouge.jpg" alt="timbre rouge">';
+					 }
+					else {
+						echo '<img class="logo" src="timbreVert.jpg" alt="timbre vert">';
+					}
+				?>
+			</div>
+		</section>
+		<div id="exped">
+			<p><?php echo $_POST["lastnameExp"].' '.$_POST["firstnameExp"] ?></p>
+			<p><?php echo $_POST["adressExp"]?></p>
+			<p><?php echo $_POST["mailExp"]?></p>
 		</div>
-		<div id="cachet">
-			<img class="logo" src="confidential.png" alt="Tampon confidentiel">
-			<!-- <?php 
-				if (checkbox) {
-					echo '<img class="logo" src="confidential.png" alt="Tampon confidentiel">';
-				}
-			?>	 -->	
+		<div id="dest">
+			<p><?php echo $_POST["lastnameDest"].' '.$_POST["firstnameDest"] ?></p>
+			<p><?php echo $_POST["adressDest"]?></p>
+			<p><?php echo $_POST["mailDest"]?></p>
 		</div>
 	</section>
-	<div id="exped">
-		<p>nom de la personne</p>
-		<p>adresse</p>
-		<p>mail</p>
-	</div>
-	<div id="dest">
-		<p>nom de la personne</p>
-		<p>adresse</p>
-		<p>mail</p>
-	</div>
 </body>
 </html>
