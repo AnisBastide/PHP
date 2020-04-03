7215a5a33369ae8e632822b836ee9129b447f3f3
anis bastide <anis.bastide@gmail.com>
2020-04-03 10:11:12 +0200

    WIP

--- portions/laposte.php
+++ portions/laposte.php
@@ -8,25 +8,77 @@
 	<?php 
 		include ("header.php");
 		require "../autoform.php";
+		require "../DB.php";
 
 	?>
 
 	<h2>La Poste</h2>
+
+    <form method="post">
     <h4>US16</h4>
-	<form method="POST" action="template.php" target="_BLANK">
 		<?php
+        ini_set('display_errors', 1);
+        $form = new autoform();
+        $dbh = new request('root', 'root', 'php', 'mysql', 'localhost');
+        $tab = $dbh->getColumns("firstname", "contact");
+        $form->getInputList("expName", "expName", $tab);
+        $form->getInputList("destName", "destName", $tab);
+        $form->getInputSubmit("submit");
 
+        echo '</form>';
+        echo '<form method"post" action="templateDynamique.php" target="_BLANK">';
+        if(!empty($_POST)){
+            $list = $dbh->getWhere("contact", "firstname=" . "'" . $_POST["expName"] . "'");
+        }
+        if(!empty($list)){
+            $lastName = $list["lastname"];
+            $firstName = $list["firstname"];
+            $userDate = $list["date"];
+            $gender = $list["gender"];
+            $mail = $list["mail"];
+            $address = $list["address"];
+            $form->getInputTextValue("lastName", "lastnameExp", $lastName);
+            $form->getInputTextValue("firstName", "firstnameExp", $firstName);
+            $form->getInputTextValue("date", "dateExp", $userDate);
+            $form->getInputTextValue("gender", "genderExp", $gender);
+            $form->getInputTextValue("mail", "mailExp", $mail);
+            $form->getInputTextValue("address", "addressExp", $address);
+        }
+        if(!empty($_POST)){
+            $list = $dbh->getWhere("contact", "firstname=" . "'" . $_POST["destName"] . "'");
+        }
+        if(!empty($list)){
+            $lastName = $list["lastname"];
+            $firstName = $list["firstname"];
+            $userDate = $list["date"];
+            $gender = $list["gender"];
+            $mail = $list["mail"];
+            $address = $list["address"];
 
-		$form = new autoform();
+            $form->getInputTextValue("lastName", "lastnameDest", $lastName);
+            $form->getInputTextValue("firstName", "firstnameDest", $firstName);
+            $form->getInputTextValue("date", "dateDest", $userDate);
+            $form->getInputTextValue("gender", "genderDest", $gender);
+            $form->getInputTextValue("mail", "mailDest", $mail);
+            $form->getInputTextValue("address", "addressDest", $address);
+            $form->getInputRadio("Envoi prioritaire", "priority", ["Oui", "Non"]);
+            $form->getInputRadio("Envoi confidentiel", "confidential", ["Oui", "Non"]);
+            $form->getInputTextArea("text","text");
+
+            $form->getInputSubmit("valider");
+            var_dump($_POST);
+
+        }
 		?>
+    </form>
+        <h4>US15</h4>
 		<h4>Expéditeur : </h4>
+    <form method="POST" action="template.php" target="_BLANK">
 		<?php
 		$form->getInputText('Nom', 'lastnameExp');
 		$form->getInputText('Prénom', 'firstnameExp');
 		$form->getInputText('Adresse', 'adressExp');
 		$form->getInputText('Adresse mail', 'mailExp');
-		// $form->getInputList("Expéditeur", "expeditor", $req->getColumns("contact", "lastname"));
-		// $form->getInputList("Destinataire", "dest", $req->getColumns("contact", "lastname"));
 		?>
 		<h4>Destinataire : </h4>
 		<?php
---
+++ portions/templateDynamique.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Preview</title>
+    <link rel="stylesheet" href="templateStyle.css"/>
+</head>
+
+<body>
+<h1>Aperçu de la lettre</h1>
+<section id="lettre">
+    <section id="prioAndCachet">
+        <div id="cachet">
+            <?php
+            var_dump($_POST);
+            ini_set('display_errors', 1);
+            if ($_POST["confidential"]=="Oui") {
+                echo '<img class="logo" src="confidential.png" alt="Tampon confidentiel">';
+            }
+            ?>
+        </div>
+        <div id="priority">
+            <?php
+            if ($_POST["priority"]=="Oui") {
+                echo '<img class="logo" src="timbreRouge.jpg" alt="timbre rouge">';
+            }
+            else {
+                echo '<img class="logo" src="timbreVert.jpg" alt="timbre vert">';
+            }
+            ?>
+        </div>
+    </section>
+    <div id="exped">
+        <p><?php echo $_POST["lastnameExp"].' '.$_POST["firstnameExp"] ?></p>
+        <p><?php echo $_POST["adressExp"]?></p>
+        <p><?php echo $_POST["mailExp"]?></p>
+    </div>
+    <div id="dest">
+        <p><?php echo $_POST["lastnameDest"].' '.$_POST["firstnameDest"] ?></p>
+        <p><?php echo $_POST["adressDest"]?></p>
+        <p><?php echo $_POST["mailDest"]?></p>
+    </div>
+    <p><?php
+        echo $_POST["text"];
+        ?></p>
+</section>
+</body>
+</html>
